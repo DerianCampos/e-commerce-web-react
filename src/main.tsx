7bd4800275935650d5419898c3f8847a5971f471
deriancampos <71363391+DerianCampos@users.noreply.github.com>
2026-08-16 18:16:08 -0600

    ref: refactoring some config files

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,6 @@ import AntDesignConfig from "@/app/theme/ant-design-config";
 import App from "@/app/App";
 import { store } from "@/app/store/store";
 import "@/styles/global.scss";
-import "@/styles/animations.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
