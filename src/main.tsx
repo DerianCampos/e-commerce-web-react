991951d487958566f024ab5fe2a6427a3ab86b65
deriancampos <71363391+DerianCampos@users.noreply.github.com>
2026-08-31 02:20:43 -0600

    chore: import some css

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/600.css";
-import "@fontsource/inter/700.css";
+import "@fontsource/anton/400.css";
+import "@fontsource/archivo-narrow/400.css";
+import "@fontsource/archivo-narrow/600.css";
+import "@fontsource/archivo-narrow/700.css";
+import "@fontsource/jetbrains-mono/500.css";
 import AntDesignConfig from "@/app/theme/ant-design-config";
 import App from "@/app/App";
 import { store } from "@/app/store/store";
